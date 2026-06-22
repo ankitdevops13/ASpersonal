13a617384393acfbbf8b552f9e4a4a6085f81857
AnkitShakya98 <vimla3119@gmail.com>
2026-06-22 13:48:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -432,6 +432,63 @@ async def get_signed_videourl(url, access_token):
         except Exception as e:
             return None
 
+
+async def ankit_video_url(url, access_token):
+    
+    parent_id = url.split("parentId=")[1].split("&")[0]
+    child_id = url.split("childId=")[1]
+    if "d1d34p8vz63oiq.cloudfront.net" in url:
+        url = url.replace("d1d34p8vz63oiq.cloudfront.net", "sec-prod-mediacdn.pw.live")
+        
+    if not access_token.startswith("Bearer "):
+        access_token = f"Bearer {access_token}"
+
+    headers = {
+        'Host': 'api.penpencil.co',
+        'Authorization': access_token,
+        'Client-Id': '5eb393ee95fab7468a79d189',
+        'Client-Type': 'WEB',
+        'Client-Version': '200',
+        'Randomid': '8ffa361e-4cc7-4948-89e8-72e552ac5460',
+        'Devicetype': 'mobile',
+        'Networktype': '3g',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Origin': 'https://www.pw.live',
+        'Referer': 'https://www.pw.live/',
+        'X-Sdk-Version': '0.0.20'
+    }
+
+    signing_url = (
+        f"https://api.penpencil.co/v1/videos/video-url-details"
+        f"?type=BATCHES&videoContainerType=DASH&reqType=query"
+        f"&childId={child_id}&parentId={parent_id}&clientVersion=201"
+    )
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(signing_url, headers=headers) as response:
+                if response.status == 200:
+                    res_json = await response.json()
+                    data_obj = res_json.get("data", {})
+
+                    signed_url = data_obj.get("link") or data_obj.get("videoUrl") or data_obj.get("signedUrl")
+
+                    if signed_url:
+                        # If signed_url is just query parameters, append to base MPD URL
+                        if signed_url.startswith("?"):
+                            return signed_url  # Return only the query parameters
+                        return signed_url
+                    else:
+                        return None
+                else:
+                    return None
+        except Exception as e:
+            return None
+
+
 async def get_credit_name(bot, m, editable, user_id, user_first_name, user_username, user_mention):
     owner_credit = f"𝐀𝐧𝐤𝐢𝐭 𝐒𝐡𝐚𝐤𝐲𝐚™🇮🇳"
     credit_options = (
@@ -913,7 +970,7 @@ async def upload(bot: Client, m: Message):
             
                 
             elif '/master.mpd' in url or "d1d34p8vz63oiq.cloudfront.net" in url or "parentId=" in url or "childId=" in url:
-                video_url = await get_signed_videourl(url, access_token)
+                video_url = await ankit_video_url(url, access_token)
                 print("PW Signed Url:", video_url)
                 wake_player()
                 url = f"https://learnwithpw-recorded.onrender.com/play?v={video_url}"
